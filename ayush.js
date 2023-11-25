e6450a5d1c9b335d0f0c4b7441c2bfa7afb3609f
Wb-Developers <143939170+Wb-Developers@users.noreply.github.com>
2023-11-25 09:20:42 -0800

    Update ayush.js

--- ayush.js
+++ ayush.js
@@ -360,28 +360,3 @@ scrollTrigger: {
 })
 
 
-  gsap.registerPlugin(ScrollTrigger);
-
-  const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-  })
-
-  function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-  }
-
-  requestAnimationFrame(raf)
-
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.img',
-      scrub: true
-    }
-  })
-  .to('.img', {
-    stagger: .2,
-    y: -700,
-    scrub: true
-  })
